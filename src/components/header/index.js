0fd246ee5c3c2115f21acc377891273c67581beb
PhamThien <phamthien994@gmail.com>
2020-05-21 11:45:35 +0700

    Update

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -4,32 +4,51 @@ import React from "react"
 import styles from "./header.module.scss"
 import logo from "../../images/150x50.png"
 
-const Header = ({ siteTitle }) => (
-    <header className={styles.header}>
-        <div className={styles.header__fixed}>
-            <div className={styles.header__inner}>
-                <h1 className={styles.header__logo}>
-                    <Link to="/">
-                        <img src={logo} alt={siteTitle} width="150" height="50" />
-                    </Link>
-                </h1>
-                <nav className={styles.header__navi}>
-                    <ul>
-                        <li>
-                            <Link to="/" activeClassName={styles.active}>Home</Link>
-                        </li>
-                        <li>
-                            <Link to="/about/" activeClassName={styles.active}>About</Link>
-                        </li>
-                        <li>
-                            <Link to="/contact/" activeClassName={styles.active}>Contact</Link>
-                        </li>
-                    </ul>
-                </nav>
-            </div>
-        </div>
-    </header>
-)
+class Header extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            isActive: false
+        }
+    }
+
+    menuOpen = () => (
+        this.setState({ isActive: !this.state.isActive })
+    )
+    render() {
+        return (
+            <header className={styles.header}>
+                <div className={styles.header__fixed}>
+                    <div className={styles.header__inner}>
+                        <h1 className={styles.header__logo}>
+                            <Link to="/">
+                                <img src={logo} alt={this.props.siteTitle} width="150" height="50" />
+                            </Link>
+                        </h1>
+                        <nav className={`${styles.header__navi}${this.state.isActive ? ` ${styles.isActive}` : ''}`}>
+                            <ul>
+                                <li>
+                                    <Link to="/" activeClassName={styles.active}>Home</Link>
+                                </li>
+                                <li>
+                                    <Link to="/about/" activeClassName={styles.active}>About</Link>
+                                </li>
+                                <li>
+                                    <Link to="/contact/" activeClassName={styles.active}>Contact</Link>
+                                </li>
+                            </ul>
+                        </nav>
+                        <div className={`${styles.header__icon}${this.state.isActive ? ` ${styles.isActive}` : ''}`} onClick={this.menuOpen}>
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                        </div>
+                    </div>
+                </div>
+            </header>
+        )
+    }
+}
 
 Header.propTypes = {
     siteTitle: PropTypes.string,
